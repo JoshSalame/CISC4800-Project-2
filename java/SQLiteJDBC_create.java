35e3cb7ac34260381e344eee38a0b397b943b5f2
Josh Salame <yehoshuasalame@gmail.com>
2020-05-07 20:56:23 -0400

    create and insert java files working. See README file for running instructions

--- java/SQLiteJDBC_create.java
+++ java/SQLiteJDBC_create.java
@@ -6,36 +6,36 @@ public class SQLiteJDBC_create {
 		Connection c = null;
 		Statement stmt = null;
 
-		final int SIZE = args.length;
-
-		/*testing purposes only
-		for (int i = 0; i < SIZE; i++) {
-			System.out.println(args[i]);
-		}*/
-
-		String tableName = args[0];
-		String[] attributeNames = new String[SIZE/2];
-		String[] attributeTypes = new String[SIZE/2];
-		
-		//fill arrays with names and types. Start from 1 to exclude tableName arg.
-		for(int i = 1; i < SIZE/2; i++) {
-			attributeNames[i-1] = args[(i*2)-1];
-			attributeTypes[i-1] = args[i*2];
-		}
-
 		try {
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:test.db");
-			//System.out.println("Opened database successfully");
+
+			final int SIZE = args.length;
+
+	                String tableName = args[0];
+	                String[] attributeNames = new String[SIZE/2];
+	                String[] attributeTypes = new String[SIZE/2];
 	
+	                //fill arrays with names and types. Start from 1 to exclude tableName arg.
+	                for(int i = 1; i < SIZE/2 + 1; i++) {
+	                        attributeNames[i-1] = args[(i*2)-1];
+	           	        attributeTypes[i-1] = args[i*2];
+			}
+
 			stmt = c.createStatement();
 			
 			//create statement based on tableName, attribute names and types.
 	        	String sql = "CREATE TABLE " + tableName + "(";;
-			for(int i = 0; i < SIZE/2; i++) {
-				sql = sql + attributeNames[i] + " " + attributeTypes[i] + " ";
+
+			//insert first to prevent ending comma
+			sql = sql + attributeNames[0] + " " + attributeTypes[0];
+			for(int i = 1; i < SIZE/2; i++) {
+				sql = sql + ", " + attributeNames[i] + " " + attributeTypes[i];
 			}
 			sql = sql + ");";
+			
+			//print executing sql statement
+			System.out.println(sql);
 
         		stmt.executeUpdate(sql);
 			stmt.close();
--- java/SQLiteJDBC_insert.java
+++ java/SQLiteJDBC_insert.java
@@ -2,40 +2,62 @@ import java.sql.*;
 
 public class SQLiteJDBC_insert {
 
-   public static void main( String args[] ) {
-      Connection c = null;
-      Statement stmt = null;
-      
-      try {
-         Class.forName("org.sqlite.JDBC");
-         c = DriverManager.getConnection("jdbc:sqlite:test.db");
-         c.setAutoCommit(false);
-         System.out.println("Opened database successfully");
+	public static void main( String args[] ) {
+		Connection c = null;
+		Statement stmt = null;
 
-         stmt = c.createStatement();
-         String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-                        "VALUES (1, 'Paul', 32, 'California', 20000.00 );"; 
-         stmt.executeUpdate(sql);
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
 
-         sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-                  "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );"; 
-         stmt.executeUpdate(sql);
+			stmt = c.createStatement();
+			
+			final int SIZE = args.length;
 
-         sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-                  "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );"; 
-         stmt.executeUpdate(sql);
+                        String tableName = args[0];
+                        String[] attributes = new String[SIZE/2];
+                        String[] values = new String[SIZE/2];
 
-         sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-                  "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );"; 
-         stmt.executeUpdate(sql);
+                        //fill arrays with attributes and their corresponding values. Start from 1 to exclude tableName arg.
+                        for(int i = 1; i < SIZE/2 + 1; i++) {
+                                attributes[i-1] = args[(i*2)-1];
+                                values[i-1] = args[i*2];
+                        }
 
-         stmt.close();
-         c.commit();
-         c.close();
-      } catch ( Exception e ) {
-         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-         System.exit(0);
-      }
-      System.out.println("Records created successfully");
-   }
+			String sql = "INSERT INTO " + tableName + " (";
+
+			//insert first attribute name to prevent an ending comma
+			sql = sql + attributes[0];
+
+			//insert other attribute names
+			for(int i = 1; i < SIZE/2; i++) {
+				sql = sql + ", " + attributes[i]; 
+			}
+
+			//insert first value 
+			sql = sql + ") VALUES (" + values[0];
+
+			//insert other values
+			for(int i = 1; i < SIZE/2; i++) {
+				sql = sql + ", " + values[i];
+			}
+
+			sql = sql + ");";
+
+			//print executing sql statement
+			System.out.println(sql);
+
+			stmt.executeUpdate(sql);
+
+			stmt.close();
+			c.commit();
+			c.close();
+		} catch ( Exception e ) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
+		System.out.println("Records created successfully");
+	}
 }
